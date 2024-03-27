0b2f320fb1156aa9628795a2ec1ccba007fdaba1
Peti <110048668+Z3RG3@users.noreply.github.com>
2024-03-27 13:30:44 +0100

    Dependencies+Startup projects set

--- FJR3IO_HFT_2023242/FJR3IO_HFT_2023242.Endpoint/obj/Debug/net5.0/FJR3IO_HFT_2023242.Endpoint.AssemblyInfo.cs
+++ FJR3IO_HFT_2023242/FJR3IO_HFT_2023242.Endpoint/obj/Debug/net5.0/FJR3IO_HFT_2023242.Endpoint.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FJR3IO_HFT_2023242.Endpoint")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a924da23e2c3f14071e49cf1688cd02d3b6494c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3eb92ec2e9272805cdb4c5aeb2d6e2424c3df1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FJR3IO_HFT_2023242.Endpoint")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FJR3IO_HFT_2023242.Endpoint")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
