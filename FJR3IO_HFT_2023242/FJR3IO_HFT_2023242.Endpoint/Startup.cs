7977f3703fc285b4999cd551594b7fc4edcac85a
Peti <110048668+Z3RG3@users.noreply.github.com>
2024-04-26 23:17:29 +0200

    Uj Endpoint letrehozva

---
+++ FJR3IO_HFT_2023242/FJR3IO_HFT_2023242.Endpoint/Startup.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FJR3IO_HFT_2023242.Endpoint
+{
+    public class Startup
+    {
+        public class Program
+        {
+            public static void Main(string[] args)
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+
+            public static IHostBuilder CreateHostBuilder(string[] args) =>
+                Host.CreateDefaultBuilder(args)
+                    .ConfigureWebHostDefaults(webBuilder =>
+                    {
+                        webBuilder.UseStartup<Startup>();
+                    });
+        }
+    }
+}
