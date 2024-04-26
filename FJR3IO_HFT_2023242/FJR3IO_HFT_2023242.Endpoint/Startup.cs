9ecb24f4d2d0d183c69e405981ff69065ecfd879
Peti <110048668+Z3RG3@users.noreply.github.com>
2024-04-26 23:25:48 +0200

    .Endpoint - Controllers

--- FJR3IO_HFT_2023242/FJR3IO_HFT_2023242.Endpoint/Startup.cs
+++ FJR3IO_HFT_2023242/FJR3IO_HFT_2023242.Endpoint/Startup.cs
@@ -1,33 +1,82 @@
+using FJR3IO_HFT_2023242.Logic;
+using FJR3IO_HFT_2023242.Models;
+using FJR3IO_HFT_2023242.Repository;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Reflection;
+using GKJAHE_HFT_2023241.Repository;
 
 namespace FJR3IO_HFT_2023242.Endpoint
 {
     public class Startup
     {
-        public class Program
+        public Startup(IConfiguration configuration)
         {
-            public static void Main(string[] args)
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<MotorcycleDbContext>();
+
+            services.AddTransient<IRepository<Motorcycle>, MotorcycleRepository>();
+            services.AddTransient<IRepository<Manufacturer>, ManufacturerRepository>();
+            services.AddTransient<IRepository<Garage>, GarageRepository>();
+
+            services.AddTransient<IMotorcycleLogic, MotorcycleLogic>();
+            services.AddTransient<IManufacturerLogic, ManufacturerLogic>();
+            services.AddTransient<IGarageLogic, GarageLogic>();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
             {
-                CreateHostBuilder(args).Build().Run();
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "FJR3IO_HFT_2023242.Endpoint", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GKJAHE_HFT_2023241.Endpoint v1"));
             }
 
-            public static IHostBuilder CreateHostBuilder(string[] args) =>
-                Host.CreateDefaultBuilder(args)
-                    .ConfigureWebHostDefaults(webBuilder =>
-                    {
-                        webBuilder.UseStartup<Startup>();
-                    });
+            app.UseExceptionHandler(c => c.Run(async context =>
+            {
+                var exception = context.Features
+                    .Get<IExceptionHandlerPathFeature>()
+                    .Error;
+                var response = new { Msg = exception.Message };
+                await context.Response.WriteAsJsonAsync(response);
+            }));
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
